498d527b087e5e32674e57cb890872188b6512e8
Evgenii Kuznetsov <zhenkuzne@gmail.com>
2023-12-23 17:11:27 +0100

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,7 +13,24 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end();
+        break;
+      }
 
+      const readableStream = fs.createReadStream(filepath);
+
+      req.on('close', () => {
+        readableStream.close()
+      })
+
+      readableStream.on('error', () => {
+        res.statusCode = 404
+        res.end();
+      })
+
+      readableStream.pipe(res)
       break;
 
     default:
