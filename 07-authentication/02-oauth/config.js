105c35f8ea82072fc0a8e28b6bc87850466e7c42
Evgenii Kuznetsov <zhenkuzne@gmail.com>
2024-01-25 20:47:30 +0100

    add solution for 7-module 2-task

--- 07-authentication/02-oauth/config.js
+++ 07-authentication/02-oauth/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   mongodb: {
     uri: (process.env.NODE_ENV === 'test') ?
--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,21 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  try {
+    if (!email) {
+      done(null, false, 'Не указан email');
+      return;
+    }
+
+    const user = await User.findOne({email});
+
+    if (user) {
+      done(null, user);
+      return;
+    }
+
+    done(null, await User.create({email, displayName}));
+  } catch (error) {
+    done(error);
+  }
 };
