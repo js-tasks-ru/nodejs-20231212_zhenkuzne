c02d731084d4bff646ab633c46883d5320d92796
Evgenii Kuznetsov <zhenkuzne@gmail.com>
2024-01-31 21:25:08 +0100

    add solution for 8-module 2-task

--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,8 +1,33 @@
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const {user, request} = ctx;
+  const {product, phone, address} = request.body;
+
+  const order = await Order.create({
+    user: user.id,
+    product,
+    phone,
+    address,
+  });
+
+  await order.populate('product');
+
+  await sendMail({
+    to: user.email,
+    subject: 'Подтверждение заказа',
+    locals: order,
+    template: 'order-confirmation',
+  });
+
+  ctx.body = {order: order.id};
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const {user} = ctx;
+  const orders = await Order.find({user: user.id});
+
+  ctx.body = {orders: orders.map(mapOrder)};
 };
